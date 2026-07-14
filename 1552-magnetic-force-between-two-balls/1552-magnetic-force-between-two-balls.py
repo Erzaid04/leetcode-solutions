42c8fb5207fc5f7994cef2f31a05177c6a4fb9d1
Zaid kakar <erzaidkakar@gmail.com>
2026-07-14 12:53:11 +0530

    Time: 899 ms (9.29%), Space: 20.4 MB (25.66%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
@@ -8,7 +8,7 @@ class Solution(object):
         position.sort()
         l = 1
         r = position[-1] - position[0]
-
+        ans = -1
         def canPlace(dist):
             count = 1
             last = position[0]
@@ -25,10 +25,11 @@ class Solution(object):
         while l<=r:
             mid = (l+r)//2
             if canPlace(mid):
+                ans = mid
                 l = mid+1
             else:
                 r = mid-1
-        return r
+        return ans
 
 
         
