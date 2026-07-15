faec446f86803142380fb0fc2dcd7da39a43a8d6
Zaid kakar <erzaidkakar@gmail.com>
2026-07-15 19:41:31 +0530

    Time: 3 ms (59.39%), Space: 12.5 MB (85.82%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -5,13 +5,25 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        merged = nums1+nums2
-        merged.sort()
+        l = 0
+        r = 0
+        merged = []
+        while l < len(nums1) and r < len(nums2):
+            if nums1[l] < nums2[r]:
+                merged.append(nums1[l])
+                l+=1
+            else:
+                merged.append(nums2[r])
+                r+=1 
+        while l < len(nums1):
+            merged.append(nums1[l])
+            l+=1
+        while r < len(nums2):
+            merged.append(nums2[r])
+            r+=1
         n = len(merged)
-        mid = 0
-        if n%2==1:
-           
+        if n %2==1:
             return merged[n//2]
         else:
-            return (merged[n//2-1]+merged[n//2])/2.0
+            return (merged[n//2-1] + merged[n//2])/2.0
 
