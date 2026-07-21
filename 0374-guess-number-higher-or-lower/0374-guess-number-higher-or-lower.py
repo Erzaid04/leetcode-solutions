519ba1a41552f7f27b706f95b506a35b4598902f
Zaid kakar <erzaidkakar@gmail.com>
2026-07-21 19:07:14 +0530

    Time: 36 ms (93.68%), Space: 19.2 MB (28.63%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
@@ -4,10 +4,9 @@ class Solution:
          r = n
          while l<=r:
             mid = l + (r-l)//2
-            res= guess(mid)
-            if res == 0:
+            if guess(mid)== 0:
                 return mid
-            elif res == -1:
+            elif guess(mid)== -1:
                 r=mid-1
             else:
                 l=mid+1
