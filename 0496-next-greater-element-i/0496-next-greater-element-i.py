3d713ec88d2d730bd22de788a89ba38853fa5a69
Zaid kakar <erzaidkakar@gmail.com>
2026-08-04 13:23:46 +0530

    Time: 3 ms (40.65%), Space: 19.3 MB (67.63%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,20 +1,22 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        nge = {}
         ans = []
-        for i in range(len(nums1)):
-            found = False
-            for j in range(len(nums2)):
-                if nums1[i] == nums2[j]:
-                    for k in range(j+1,len(nums2)):
-                        if nums1[i]<nums2[k]:
-                            ans.append(nums2[k])
-                            found = True
-                            break
-                    break
-            if not found:
-                ans.append(-1)
+        for i in range(len(nums2)):
+            while stack and stack[-1]<nums2[i]:
+                x = stack.pop()
+                num = nums2[i]
+                nge[x] = num
+            stack.append(nums2[i])
+        while stack:
+                x = stack.pop()
+                nge[x] = -1   
 
-                       
-        return ans
 
         
+        for num in nums1:
+            ans.append(nge[num])
+        return ans
+
+           
