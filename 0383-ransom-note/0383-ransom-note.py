4f3af08620d5a4c1ca115d455de4506fe588c211
Zaid kakar <erzaidkakar@gmail.com>
2026-08-28 18:32:00 +0530

    Time: 17 ms (66.04%), Space: 19.6 MB (33.67%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        cnt = {}
+
+        for ch in magazine:
+            cnt[ch] = cnt.get(ch, 0) + 1
+
+        for ch in ransomNote:
+            if cnt.get(ch, 0) == 0:
+                return False
+            cnt[ch] -= 1
+
+        return True
